649d0dc7e0258c3e9a312b17a5dc2af8498855ff
medonagy45 <medonagy45@gmail.com>
2021-03-20 23:45:14 +0200

    changing the testing to relay on chai-http

--- test/machines.test.js
+++ test/machines.test.js
@@ -1,58 +1,62 @@
 var expect = require("chai").expect;
-var request = require("request");
+var chai = require("chai"),
+  chaiHttp = require("chai-http");
 const {
   model,
   product_type,
   water_line_compatible,
 } = require("../src/model/machines");
+var configuredApp = require("../app.config");
 
 describe("Testing Pods", function () {
-  var url = "http://localhost:3000/machines/";
+  chai.use(chaiHttp);
+  var requester = chai.request(configuredApp).keepOpen();
+  var url = "/machines/";
   it("All large machines", function (done) {
     var propertiesObject = { product_type: product_type.COFFEE_MACHINE_LARGE };
-    request(
-      { url: url, qs: propertiesObject },
-      function (error, response, body) {
-        let items = JSON.parse(body);
-
-        expect(response.statusCode).to.equal(200);
-        console.log(items.machines);
-        expect(items.machines).to.deep.equal(["CM101", "CM102", "CM103"]);
+    requester
+      .get(url)
+      .query(propertiesObject)
+      .send()
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body.machines).to.deep.equal(["CM101", "CM102", "CM103"]);
         done();
-      }
-    );
+      });
   });
+
   it("All ESPRESSO machines", function (done) {
     var propertiesObject = { product_type: product_type.ESPRESSO_MACHINE };
-    request(
-      { url: url, qs: propertiesObject },
-      function (error, response, body) {
-        let items = JSON.parse(body);
-
-        expect(response.statusCode).to.equal(200);
-        console.log(items.machines);
-        expect(items.machines).to.deep.equal(["EM001", "EM002", "EM003"]);
+    requester
+      .get(url)
+      .query(propertiesObject)
+      .send()
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body.machines).to.deep.equal(["EM001", "EM002", "EM003"]);
         done();
-      }
-    );
+      });
   });
+
   it("All water_line_compatible machines", function (done) {
     var propertiesObject = { water_line_compatible: water_line_compatible };
-    request(
-      { url: url, qs: propertiesObject },
-      function (error, response, body) {
-        let items = JSON.parse(body);
-
-        expect(response.statusCode).to.equal(200);
-        console.log(items.machines);
-        expect(items.machines).to.deep.equal([
+    requester
+      .get(url)
+      .query(propertiesObject)
+      .send()
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body.machines).to.deep.equal([
           "CM003",
           "CM102",
           "CM103",
           "EM003",
         ]);
+        requester.close();
         done();
-      }
-    );
+      });
   });
 });
--- test/pods.test.js
+++ test/pods.test.js
@@ -1,20 +1,23 @@
-const expect = require("chai").expect;
-const e = require("express");
-const request = require("request");
+var { expect } = require("chai");
+var chai = require("chai"),
+  chaiHttp = require("chai-http");
+var configuredApp = require("../app.config");
 const { coffee_flavor, pack_size, product_type } = require("../src/model/pods");
 
 describe("Testing Pods", function () {
-  var url = "http://localhost:3000/pods/";
+  chai.use(chaiHttp);
+  var requester = chai.request(configuredApp).keepOpen();
+  var url = "/pods/";
   it("All large pods", function (done) {
     var propertiesObject = { product_type: product_type.COFFEE_POD_LARGE };
-    request(
-      { url: url, qs: propertiesObject },
-      function (error, response, body) {
-        let items = JSON.parse(body);
-
-        expect(response.statusCode).to.equal(200);
-        console.log(items.pods);
-        expect(items.pods).to.deep.equal([
+    requester
+      .get(url)
+      .query(propertiesObject)
+      .send()
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body.pods).to.deep.equal([
           "CP101",
           "CP103",
           "CP111",
@@ -27,38 +30,38 @@ describe("Testing Pods", function () {
           "CP143",
         ]);
         done();
-      }
-    );
+      });
   });
   it("All espresso vanilla pods", function (done) {
     var propertiesObject = {
       coffee_flavor: coffee_flavor.COFFEE_FLAVOR_VANILLA,
       product_type: product_type.ESPRESSO_POD,
     };
-    request(
-      { url: url, qs: propertiesObject },
-      function (error, response, body) {
-        let items = JSON.parse(body);
+    requester
+      .get(url)
+      .query(propertiesObject)
+      .send()
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
 
-        expect(response.statusCode).to.equal(200);
-        console.log(items.pods);
-        expect(items.pods).to.deep.equal(["EP003", "EP005", "EP007"]);
+        expect(res.body.pods).to.deep.equal(["EP003", "EP005", "EP007"]);
         done();
-      }
-    );
+      });
   });
   it("All small pods", function (done) {
     var propertiesObject = {
       product_type: product_type.COFFEE_POD_SMALL,
     };
-    request(
-      { url: url, qs: propertiesObject },
-      function (error, response, body) {
-        let items = JSON.parse(body);
+    requester
+      .get(url)
+      .query(propertiesObject)
+      .send()
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
 
-        expect(response.statusCode).to.equal(200);
-        console.log(items.pods);
-        expect(items.pods).to.deep.equal([
+        expect(res.body.pods).to.deep.equal([
           "CP001",
           "CP003",
           "CP011",
@@ -71,23 +74,23 @@ describe("Testing Pods", function () {
           "CP043",
         ]);
         done();
-      }
-    );
+      });
   });
   it("All 7 pods", function (done) {
     var propertiesObject = {
       pack_size: pack_size.seven_dozen,
     };
-    request(
-      { url: url, qs: propertiesObject },
-      function (error, response, body) {
-        let items = JSON.parse(body);
+    requester
+      .get(url)
+      .query(propertiesObject)
+      .send()
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
 
-        expect(response.statusCode).to.equal(200);
-        console.log(items.pods);
-        expect(items.pods).to.deep.equal(["EP007", "EP017"]);
+        expect(res.body.pods).to.deep.equal(["EP007", "EP017"]);
+        requester.close();
         done();
-      }
-    );
+      });
   });
 });
